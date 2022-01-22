63613c7beb22857285625bdb3d46192a6835c376
OtterBub <gamepsk@hanmail.net>
2022-01-22 11:12:56 +0900

    Update annotation

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "framework/gameframe.hpp"
 #include "framework/scene/scene.hpp"
 
-// by Park SungKyoung
-// 2022/01/21 
+// Created by Park SungKyoung
+// 2022/01/21
 // 
 // \033[5;6H -> Terminal Cursor Position Control
 // by https://rosettacode.org/wiki/Terminal_control/Cursor_positioning#C.2FC.2B.2B
@@ -15,7 +15,6 @@ int main (int argc, char *argv[])
     std::cout << "Hello Game FrameWork!" << std::endl;
 
     GameFrame frame = GameFrame();
-
     frame.Run();
     
     return 0;
